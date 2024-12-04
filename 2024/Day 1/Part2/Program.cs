f2f192b2dffd4c7bde5034bae7fb690c3ce8e440
Marcus Helvig <git@marcus.helvig.net>
2024-12-04 16:38:49 +1100

    Complete part 2

---
+++ 2024/Day 1/Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+internal static partial class Program
+{
+    // look for a string of digits, followed by a equence of spaces, and another string of digits
+    [GeneratedRegex(@"^(\d+) +(\d+)$")]
+    private static partial Regex lineRegex();
+
+
+    private static async Task Main(string[] args)
+    {
+        var list1=new List<int>();
+        var list2=new List<int>();
+
+        var input = File.ReadLinesAsync("input.txt", CancellationToken.None);
+
+        // Becasue we sort the lists
+        await foreach (var line in input)
+        {
+            if (lineRegex().IsMatch(line))
+            {
+                var matches = lineRegex().Match(line);
+                list1.Add(int.Parse(matches.Groups[1].Value));
+                list2.Add(int.Parse(matches.Groups[2].Value));
+            }
+            else
+            {
+                Console.WriteLine($"Line: \"{line}\".");
+            }
+        }
+
+        var accumulator=list1
+            .Select(x=> x * list2.Where(y=>y==x).Count()).Sum();
+        Console.WriteLine($"The difference is {accumulator}");
+    }
+}
