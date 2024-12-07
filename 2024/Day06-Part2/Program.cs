d2f979674c0c348c38433563c96c6d2856e0c894
Marcus Helvig <git@marcus.helvig.net>
2024-12-07 13:21:18 +1100

    Rather than brute forcing every square, the only blocks that would affect the path are actually *on* the path of the guard.
    This gives us a 4x speed up, and makes me feel better.

--- 2024/Day06-Part2/Program.cs
+++ 2024/Day06-Part2/Program.cs
@@ -16,11 +16,10 @@ internal static partial class Program {
 
 #pragma warning disable IDE0060 // Remove unused parameter
     private static async Task Main(string[] args) {
-        var lines = new List<char[]>();
+        var mapRows = new List<char[]>();
 
         var validBlocks = 0;
         var badBlocks = 0;
-        var invalidBlockPlacements = 0;
 
         var start = new Location(-1, -1);
 
@@ -30,12 +29,12 @@ internal static partial class Program {
         // data first, then process it.
         await foreach (var line in input) {
             if (line.Trim().Length > 0) {
-                lines.Add(line.Trim().ToCharArray());
+                mapRows.Add(line.Trim().ToCharArray());
             }
 
             if (StartField().IsMatch(line)) {
                 var match = StartField().Match(line);
-                start = (lines.Count - 1, match.Index);
+                start = (mapRows.Count - 1, match.Index);
             }
         }
 
@@ -43,53 +42,72 @@ internal static partial class Program {
             throw new InvalidDataException("No starting location");
         }
 
-        for (var block = new Location(0, 0); block.row <= lines.Count - 1; block.row++) {
-            for (block.col = 0; block.col <= lines[block.row].Length - 1; block.col++) {
-                if (lines[block.row][block.col] != '.') {
-                    invalidBlockPlacements++;
-                    continue;
-                }
+        var origionalLocation = start;
+        var origionalDirection = Direction.Up;
 
-                var direction = Direction.Up;
+        var path = new HashSet<Location> { };
 
-                var history = new HashSet<History> { };
-                var location = start;
-                var loop = false;
+        while (true) {
+            _ = path.Add(origionalLocation);
+            var target = origionalDirection! switch {
+                Direction.Up => new Location(origionalLocation.row - 1, origionalLocation.col),
+                Direction.Down => new Location(origionalLocation.row + 1, origionalLocation.col),
+                Direction.Right => new Location(origionalLocation.row, origionalLocation.col + 1),
+                Direction.Left => new Location(origionalLocation.row, origionalLocation.col - 1),
+                _ => throw new InvalidDataException("Invalid direction"),
+            };
+            if (target.row < 0 || target.col < 0 || target.row >= mapRows.Count || target.col >= mapRows[0].Length) {
+                break;
+            }
 
-                while (true) {
-                    if (!history.Add(new History(location, direction))) {
-                        loop = true;
-                        break;
-                    }
-
-                    var target = direction! switch {
-                        Direction.Up => new Location(location.row - 1, location.col),
-                        Direction.Down => new Location(location.row + 1, location.col),
-                        Direction.Right => new Location(location.row, location.col + 1),
-                        Direction.Left => new Location(location.row, location.col - 1),
-                        _ => throw new InvalidDataException("Invalid direction"),
-                    };
-
-                    if (target.row < 0 || target.col < 0 || target.row >= lines.Count || target.col >= lines[0].Length) {
-                        badBlocks++;
-                        break;
-                    }
-
-                    if (lines[target.row][target.col] == '#' || target == block) {
-                        direction = direction == Direction.Left ? Direction.Up : direction + 1;
-                    }
-                    else {
-                        location = target;
-                    }
-                }
-
-                if (loop) {
-                    validBlocks++;
-                }
+            if (mapRows[target.row][target.col] == '#') {
+                origionalDirection = origionalDirection == Direction.Left ? Direction.Up : origionalDirection + 1;
+            }
+            else {
+                origionalLocation = target;
             }
         }
 
-        Console.WriteLine($"Blocks causing loops: {validBlocks} (Not Tried: {invalidBlockPlacements}, not loop: {badBlocks})");
+        foreach (var block in path) {
+            var direction = Direction.Up;
+
+            var history = new HashSet<History> { };
+            var location = start;
+            var loop = false;
+
+            while (true) {
+                if (!history.Add(new History(location, direction))) {
+                    loop = true;
+                    break;
+                }
+
+                var target = direction! switch {
+                    Direction.Up => new Location(location.row - 1, location.col),
+                    Direction.Down => new Location(location.row + 1, location.col),
+                    Direction.Right => new Location(location.row, location.col + 1),
+                    Direction.Left => new Location(location.row, location.col - 1),
+                    _ => throw new InvalidDataException("Invalid direction"),
+                };
+
+                if (target.row < 0 || target.col < 0 || target.row >= mapRows.Count || target.col >= mapRows[0].Length) {
+                    badBlocks++;
+                    break;
+                }
+
+                if (mapRows[target.row][target.col] == '#' || target == block) {
+                    direction = direction == Direction.Left ? Direction.Up : direction + 1;
+                }
+                else {
+                    location = target;
+                }
+            }
+
+            if (loop) {
+                validBlocks++;
+            }
+        }
+
+        Console.WriteLine($"Blocks causing loops: {validBlocks} (No loop: {badBlocks})");
     }
 #pragma warning restore IDE0060 // Remove unused parameter
 }
